61c97750af247d6d59828d224a05b72155143036
OutOfBrain <outofbrain@gmail.com>
2012-11-27 00:50:46 +0100

    first draft of room structure

--- textadventure.dart
+++ textadventure.dart
@@ -1,15 +1,128 @@
+import 'dart:io';
 
-class Room {
+/**
+ * ROOMS
+ */
+
+/**
+ * base room
+ */
+abstract class Room {
+  String description;
+  Room match(String direction);
 }
 
 
+class RoomStore extends Room implements Room {
+  RoomStore() {
+    this.description = 'Store Room';
+  }
+
+  Room match(String direction) {
+    switch (direction) {
+      case 'west':
+      case 'door':
+      case 'closet':
+        return new RoomHide();
+
+      case 'east':
+      case 'kitchen':
+        return new RoomKitchen();
+
+      case 'south':
+      case 'hall':
+        return new RoomHall();
+    }
+
+    return null;
+  }
+}
+
+
+class RoomHide extends Room implements Room {
+  RoomHide() {
+    this.description = 'Hidden Room';
+  }
+
+  Room match(String direction) {
+
+  }
+}
+
+
+class RoomKitchen extends Room implements Room {
+  RoomKitchen() {
+    this.description = 'Kitchen';
+  }
+
+  Room match(String direction) {
+
+  }
+}
+
+
+class RoomHall extends Room implements Room {
+  RoomHall() {
+    this.description = 'Hall';
+  }
+
+  Room match(String direction) {
+
+  }
+}
+
+
+class RoomStudy extends Room implements Room {
+  RoomStudy() {
+    this.description = 'Study Room';
+  }
+
+  Room match(String direction) {
+
+  }
+}
+
+
+class RoomCloset extends Room implements Room {
+  RoomCloset() {
+    this.description = 'Closet';
+  }
+
+  Room match(String direction) {
+
+  }
+}
+
+
+/**
+ * PLAYER
+ */
 class Player {
-  Room room;
+  Room location;
 
-  Player(Room this.room);
+  Player() {
+    this.location= new RoomStore();
+  }
+
+  go(direction) {
+    Room newRoom = location.match(direction);
+    if (newRoom != null) {
+      this.location = newRoom;
+      print('you are now in ${this.location.description}');
+    }
+  }
 }
+
 
 
 void main() {
+  Player player = new Player();
+  StringInputStream input = new StringInputStream(stdin);
 
+  input.onLine = () {
+    String line = input.readLine();
+    if (line.startsWith('go')) {
+      player.go(line.substring(line.indexOf('go ')+3));
+    }
+  };
 }
